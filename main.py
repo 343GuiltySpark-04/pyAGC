f02264689104045b6865539d7132fd0f1608f8c5
captain-rainbow <67162230+captain-rainbow@users.noreply.github.com>
2021-01-28 18:45:32 +0100

    Add TODO's, add AUG function, add more description

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 # PyAGC
-# Victor Mout
+# Victor Mout, 2021
+# GNU License
+#
+# C U R R E N T  I N S T R U C T I O N S #
+# AD
+# ADS
+# CA
+# TS
+# XCH
+# AUG
 
-# Define RAM and ROM
-class memory:
-    UNSWITCHED_WRITABLE_MEMORY = [0] * 767
-    SWITCHED_WRITABLE_BANK_MEMORY1 = [0] * 255
-    SWITCHED_UNWRITABLE_BANK_MEMORY1 = [0] * 1024
-    UNSWITCHED_UNWRITABLE_MEMORY = [0] * 2048
-    ACTIVE_UNWRITABLE_BANK = SWITCHED_UNWRITABLE_BANK_MEMORY1
-    ACTIVEBANK = SWITCHED_WRITABLE_BANK_MEMORY1
-    MEMORY = UNSWITCHED_WRITABLE_MEMORY + ACTIVEBANK + ACTIVE_UNWRITABLE_BANK + UNSWITCHED_UNWRITABLE_MEMORY
+# TODO: Add line by line file reader
+# TODO: Finish all instructions
+# TODO: Add function logic
+# TODO: Add I/O logic
+# TODO: Add DSKY output
+# TODO: Finish registers
+# TODO: Add nessecary ROM data
+
+
+
+# Define Memory and ROM
+class Memory:
+    UNSWITCHED_WRITABLE_Memory = [0] * 767
+    SWITCHED_WRITABLE_BANK_Memory1 = [0] * 255
+    SWITCHED_UNWRITABLE_BANK_Memory1 = [0] * 1024
+    UNSWITCHED_UNWRITABLE_Memory = [0] * 2048
+    ACTIVE_UNWRITABLE_BANK = SWITCHED_UNWRITABLE_BANK_Memory1
+    ACTIVEBANK = SWITCHED_WRITABLE_BANK_Memory1
+    Memory = UNSWITCHED_WRITABLE_Memory + ACTIVEBANK + ACTIVE_UNWRITABLE_BANK + UNSWITCHED_UNWRITABLE_Memory
     IO_SPACE = [0] * 511
 
 # CPU object
@@ -23,43 +42,66 @@ class CPU(object):
 
     def processInstruction(self, instruction: str):
         splitInstruction = instruction.split()
+        arg =  Memory.Memory[int(splitInstruction[1], 0)]
 
-        # AD instruction. Adds the contents of a given memory cell and writes it into the accumulator.
+        
+        # AD instruction. Adds the contents of a given Memory cell and writes it into the accumulator.
         if instruction.__contains__('AD') and not instruction.__contains__('ADS'):
-            self.A = self.A + int(splitInstruction[1], 0)
+            self.A = self.A + Memory.Memory[int(splitInstruction[1], 0)]
             # DEBUG
             print("(AD) Register A: " + str(self.A))
-        # ADS instruction. Adds the contents of a given memory cell and writes it into the accumulator.
+
+
+        # ADS instruction. Adds the contents of a given Memory cell and writes it into the accumulator.
         if instruction.__contains__('ADS'):
-            memory.RAM[int(splitInstruction[1], 0)] = memory.RAM[int(splitInstruction[1], 0)] + self.A
+            arg =arg + self.A
             # DEBUG
-            print("(ADS) Memory Address: " + str(memory.RAM[int(splitInstruction[1], 0)]))
-        # CA instruction. Copies contents of a given memory cell into the accumulator.
+            print("(ADS) Memory Address: " + str(arg))
+
+
+        # CA instruction. Copies contents of a given Memory cell into the accumulator.
         if instruction.__contains__('CA'):
-            self.A = memory.RAM[int(splitInstruction[1], 0)]
+            self.A =  arg
             # DEBUG
             print("(CA) Register A: " + str(self.A))
-        # TS instruction. Sets contents of a given memory cell to the contents of the accumulator.
+
+
+        # TS instruction. Sets contents of a given Memory cell to the contents of the accumulator.
         if instruction.__contains__('TS'):
-            memory.RAM[int(splitInstruction[1], 0)] = self.A
+            Memory.Memory[int(splitInstruction[1], 0)] = self.A
             # DEBUG
-            print("(TS) New memory value: " + str(memory.RAM[int(splitInstruction[1], 0)]))
-        # XCH instruction. Swaps contents of the accumulator with given memory address.
+            print("(TS) New Memory value: " + str(Memory.Memory[int(splitInstruction[1], 0)]))
+
+
+        # XCH instruction. Swaps contents of the accumulator with given Memory address.
         if instruction.__contains__('XCH'):
             self.Q = self.A
-            self.A = memory.RAM[int(splitInstruction[1], 0)]
-            memory.RAM[int(splitInstruction[1], 0)] = self.Q
+            self.A = arg
+            arg = self.Q
             # DEBUG
             print("(XCH) Accumulator: ", str(self.A))
-            print("(XCH) Memory address: ", memory.RAM[int(splitInstruction[1], 0)])
+            print("(XCH) Memory address: ",arg)
 
 
-# Main instruction
+        # AUG instruction. Increments a positive value an negates a negative value.
+        if instruction.__contains__('AUG'):
+            if (arg> 0):
+                arg = arg +1
+            if (arg<0):
+                arg = arg -1
+            else:
+                pass
+            # DEBUG
+            print("(AUG) New Memory value: " + str(arg))
+
+# Main function
 def main():
-
-    CPU.A = 0x10
-    memory.RAM[0x200] = 0x20
-    CPU.processInstruction(instruction="", self=CPU)
+    Memory.Memory[0x1FF] = 0x10
+    Memory.Memory[0x200] = 0x10
+    code = ["CA   0x1FF", "AD   0x200", "TS   0x1FF"]
+    x = input("Instruction > ")
+    CPU.processInstruction(instruction=x, self=CPU)
+    main()
 
 
 # name/main run check
